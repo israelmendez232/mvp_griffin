2403b548837e996ddf5754de7db3ac9c1f97e6f5
Israel Mendes <israel.mendez232@gmail.com>
2019-12-29 19:32:19 +0000

    Serving better deployment with a shell script.

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# Code to assist on deployment of the app.
+
+## Terminal:
+## > sh deploy.sh
+
+# !/bin/bash
+## SETTING UP THE ENVIROMENT:
+forever stopall
+npm install -g npm@latest
+npm install -g forever
+cd mvp_griffin/
+git pull origin master
+
+## GETTING THE FRONT READY
+cd client/
+npm install
+npm run-script build
+mv -f build ../server/src/
+
+## RUN THE SERVER
+cd ../server/
+npm install
+npm run server
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 4000
