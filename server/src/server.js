46732d49412b6b7c779340f8d2b0716e28c869c9
Israel Mendes <israel.mendez232@gmail.com>
2019-12-29 18:53:42 +0000

    Serving the code for production.

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,14 @@
 import express from 'express';
 import routes from './routes/routes';
+import path from 'path';
 require('./database');
 
 // Setup
 const app = express();
 const PORT = 4000;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(express.json());
-
-// // Connection into the Database
-// const client = new Client({
-//   user: config.db_dev.user,
-//   host: config.db_dev.host,
-//   database: config.db_dev.db,
-//   password: config.db_dev.pass,
-//   port: config.db_dev.port
-// });
-// client.connect();
 
 // Routes
 app.get('/', (req, res) => {
@@ -27,7 +17,11 @@ app.get('/', (req, res) => {
 
 app.use(routes);
 
-// Ending
+// Ending and Serving
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Listen in on http://localhost:${PORT}/`)
 });
